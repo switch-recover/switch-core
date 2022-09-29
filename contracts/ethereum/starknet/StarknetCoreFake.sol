e6ba391ae4825d5dd54c3a4be804564a9a034bd2
yeungparkhay <yeungparkhay@gmail.com>
2022-09-29 21:38:50 +0100

    added original contract in comments

--- contracts/ethereum/starknet/StarknetCoreFake.sol
+++ contracts/ethereum/starknet/StarknetCoreFake.sol
@@ -16,3 +16,24 @@ contract StarknetCoreFake {
         require(isValid, "INVALID_MESSAGE_TO_CONSUME");
     }
 }
+
+// Original contract:
+// function consumeMessageFromL2(uint256 fromAddress, uint256[] calldata payload)
+//     external
+//     override
+//     returns (bytes32)
+// {
+//     bytes32 msgHash = keccak256(
+//         abi.encodePacked(
+//             fromAddress,
+//             uint256(uint160(address(msg.sender))),
+//             payload.length,
+//             payload
+//         )
+//     );
+
+//     require(l2ToL1Messages()[msgHash] > 0, "INVALID_MESSAGE_TO_CONSUME");
+//     emit ConsumedMessageToL1(fromAddress, msg.sender, payload);
+//     l2ToL1Messages()[msgHash] -= 1;
+//     return msgHash;
+// }
